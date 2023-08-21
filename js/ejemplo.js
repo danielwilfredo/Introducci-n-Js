cc13c06f23015314e589152e11637cdd2e61eafe
Estudiante <estudiante@ricaldone.edu.sv>
2023-08-21 09:10:41 -0600

    first commit

---
+++ js/ejemplo.js
@@ -0,0 +1,57 @@
+//ciclo while
+
+/*let detener="";
+while(detener != "stop")
+{
+    detener=prompt(`Deseas seguir ejecutando el ciclo:
+    SI o stop para detener`);
+}*/
+
+//ciclo Do while
+/*let num =5;
+do{
+    alert(num);
+    num=parseInt(prompt("Ingrese un numero"));
+}
+while(num!=0)*/
+
+//Arreglos
+/*
+let notas = [];
+let suma=0;
+for(let i = 0; i<4;i++)
+{
+    let nota=parseInt(prompt("Ingrese una nota: "));
+    notas.push(nota);
+    suma +=notas[i];
+}
+
+console.log(suma);
+console.log(notas);
+
+*/
+
+//funciones
+/*
+function sumar()
+{
+    let a=parseFloat(prompt("Ingrese un numero: "));
+    let b=parseFloat(prompt("Ingrese un numero: "));
+    let suma=(a+b);
+
+    return suma;
+
+}
+*/
+let a=parseFloat(prompt("Ingrese un numero: "));
+    let b=parseFloat(prompt("Ingrese un numero: "));
+
+function sumar(a=10,b=1)
+{
+    
+    let suma=(a+b);
+
+    return suma;
+}
+
+alert(sumar(b));
